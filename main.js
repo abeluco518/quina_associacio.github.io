8f18bd66f2d8c7e7f25f78ae621d607d122abfac
abel <ababcantudo@gmail.com>
2025-12-12 19:31:49 +0100

    Updates

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ for (let number of numbers) {
     number.addEventListener('click', (e) => {
         e.preventDefault()
         const circle = e.currentTarget.querySelector(".bingo__circle");
-
-
         circle.classList.toggle('bingo__circle-on');
+        document.getElementById('number-show__number').textContent=number.querySelector('.number').textContent
     })
 }
